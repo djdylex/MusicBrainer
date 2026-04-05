29953ca5189017566c901842d40b5f42a6d158cb
DjDylex <dominicnewman022@gmail.com>
2026-04-05 13:15:54 +0100

    Added intitialization implementations

--- MusicBrain/HTMBrain.cpp
+++ MusicBrain/HTMBrain.cpp
@@ -1,28 +1,82 @@
 #include "HTMBrain.h"
 
-HTMLayer::HTMLayer(int columns, int cells, int inputRange) : columnConverter(columns, inputRange), active(columns, cells), predictive(columns, cells) {
+HTMLayer::HTMLayer(int columns, int cells, int inputRange) : columns_(columns), cells_(cells), inputRange_(inputRange), 
+	active_(columns, cells), predictive_(columns, cells), gen_(rd_()) {
+
+	segments_ = vector<vector<vector<segment>>>(
+		columns_, vector<vector<segment>>(
+			cells_, vector<segment>(
+				segmentsCount_, segment{})
+		)
+	);
+
+	createColumnConverter();
 	resetSegments();
 	resetActive();
-	resetPredictive;
+	resetPredictive();
 }
 
-// In the feed forward, input is converted to active a sparse selection of minicolumns, for which all cells in the minicolumn have the exact same receptive field. (e.g will activate on seeing the same symbol)
-void HTMLayer::createColumnConverter(int CPC) {
-	for int
+int HTMLayer::randInt(int min, int max) {
+	std::uniform_int_distribution<> dist(min, max);
+	return dist(rd_);
+}
+
+float HTMLayer::randFloat() {
+	std::uniform_real_distribution<float> dist(0, 1);
+	return dist(rd_)
+}
+
+MatrixXd DecToBinVec(int dec) {
+	int size = log2(dec);
+	int rem = dec;
+	MatrixXd vec(1, size + 1);
+	for (int i = size; i >= 0; i--) {
+		if (pow(2, i) > rem) {
+			vec(0, i) = 0;
+			continue;
+		} else {
+			rem -= pow(2, i);
+			vec(0, i) = 1;
+		}
+	}
+}
+
+// In the feed forward, input is converted to a sparse selection of minicolumns, for which all cells in the minicolumn have the exact same receptive field. (e.g will activate on seeing the same symbol)
+void HTMLayer::createColumnConverter() {
+	// convert input to binary number < no of minicolumns using some kind of or random
+	double A = (sqrt(5.0) - 1.0) / 2.0;
+	int tableSize = pow(2, columns_);
+
+	// elimnate duplicates
+	// create dictionary for these numbersive neuron matrix
+	for (int i = 0; i < inputRange_; i++) {
+		int hash = tableSize * (i * A);
+
+		columnConverter_[i] = DecToBinVec(hash);
+	}
 }
 
 void HTMLayer::feedForward(int symbol) {
 	// All cells in minicolumn share same feed forward receptive field.
-	// If a cell's feedforward is above threshold and was predicted then that cell is set as active
+	// If a cell's feedforward is above threshold and was pre
+	// on feed forward, convert binary number to actdicted then that cell is set as active
 	// If no cells in a column were predicted, then all cells are set as active
 }
 
 void HTMLayer::resetSegments() {
+	for (int col = 0; col < segments_.size(); col++) {
+		for (int cell = 0; cell < segments_[0].size(); cell++) {
+			for (int seg = 0; seg < segmentsCount_; seg++) {
+				for (int con = 0; con < maxSynapses_; con++) {
+					segments_[col][cell][seg].connections[{randInt(0, segmentsCount_), randInt(0, cells_), seg}] = randFloat();
+				}
+			}
+		}
+	}
 	// Iterate through each segment for each cell for every column
 	// Make sure each permenance value is set to zero
 	// Select s connections in each segment to have non zero permenance values, assigning a value between 0-1
 	// values more than threshold (usually 0.3) will be considered connections
-
 }
 
 void HTMLayer::pruneConnections(int pruneAmount) {
@@ -31,11 +85,17 @@ void HTMLayer::pruneConnections(int pruneAmount) {
 }
 
 void HTMLayer::resetActive() {
-	//Iterate through all active connections
-	// Set to zero
+	for (int c = 0; c < active_.cols(); c++) {
+		for (int r = 0; c < active_.rows(); r++) {
+			active_[r, c] = 0;
+		}
+	}
 }
 
 void HTMLayer::resetPredictive() {
-	// Iterate through all prediction connections
-	// Set to zero
+	for (int c = 0; c < active_.cols(); c++) {
+		for (int r = 0; c < active_.rows(); r++) {
+			predictive_[r, c] = 0;
+		}
+	}
 }
--- MusicBrain/HTMBrain.h
+++ MusicBrain/HTMBrain.h
@@ -4,23 +4,33 @@
 #include <cstdlib>
 #include <Eigen/Dense>
 #include <vector>
+#include <map>
+#include <random>
 
 using namespace std;
 using namespace Eigen;
 
+struct segment {
+	std::map<std::tuple<int, int, int>, float> connections;
+};
 
 class HTMLayer {
 private:
-	void createColumnConverter(int CellsPerCol);
-	vector<vector<MatrixXd>> segments_;
-	MatrixXd columnConverter_; // columns x inputrange
+	void createColumnConverter();
+	int randInt(int min, int max);
+	float randFloat();
+	std::random_device rd_; 
+	std::mt19937 gen_; 
+	vector<vector<vector<segment>>> segments_; // segment per cell (i, j), n segments per cell, 
+	//MatrixXd columnConverter_; // columns x inputrange
 	MatrixXd active_;
 	MatrixXd predictive_;
-	const int inputRange_;
+	std::map<int, MatrixXd> columnConverter_;
+	const int inputRange_; // e.g 36 notes
 	const int columns_ = 1024;
 	const int ffCols_ = columns_ * 0.02; // Conversion of data to minicolumns (feedforward)
 	const int cells_ = 32; // cells per column
-	const int segments_ = 128; // segments per cell
+	const int segmentsCount_ = 128; // segments per cell
 	const int maxSynapses_ = 40;
 
 public:
@@ -31,5 +41,4 @@ public:
 	void pruneConnections(int pruneAmount);
 	void feedForward(int symbol);
 };
-
 #endif
